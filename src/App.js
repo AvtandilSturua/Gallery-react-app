6a6be1f3e60504da9a1d7e0bff546e733f2ba2c6
Avto <avtandil.sturua.1@btu.edu.ge>
2023-01-22 14:56:30 +0400

    Gallery

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [image, setImage] = useState(null);
+  const [data, setData] = useState([]);
+  const addItem = () => {
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: image,
+    };
+    setData((OldList) => [...OldList, item]);
+    console.log(image);
+  };
+  const deleteItem = (id) => {
+    const newArray = data.filter((item) => item.id !== id);
+    setData(newArray);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main-container">
+        <h1>The home for your memories</h1>
+        <div className="input">
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            accept="image/png, image/jpeg"
+          />
+          <button onClick={addItem}>ADD</button>
+        </div>
+        <div className="photos-container">
+          {data.map((item) => (
+            <>
+              <div className="box">
+                <div className="image">
+                  <img width={"250px"} src={URL.createObjectURL(item.value)} />
+                </div>
+                <div className="attribute">
+                  <button onClick={() => deleteItem(item.id)}>Delete</button>
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
